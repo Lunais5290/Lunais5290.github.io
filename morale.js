74dd6123db0d157984b70c1818bcded82ab58d18
Lunais5290 <lunais5290@gmail.com>
2026-06-07 00:58:31 +0900

    Add files via upload

---
+++ morale.js
@@ -0,0 +1,66 @@
+function calculatemorale() {
+  const mode = document.getElementById("mode").value;
+  const morale = Number(document.getElementById("morale").value);
+  const time = Number(document.getElementById("time").value);
+
+  const outputlaps = document.getElementById("outputlaps");
+  const outputtime = document.getElementById("outputtime");
+
+  if (!Number.isInteger(morale) || morale < 1 || 75 <morale) {
+    alert("1~75の開始やる気値を入力してください。");
+    return;
+  }
+
+  if (!Number.isFinite(time) || time < 1) {
+    alert("1以上の周回時間を入力してください。");
+    return;
+  }
+
+  if ((mode === "terakoya" && time >= 180) || (mode === "normal" && time >= 360)) {
+    outputlaps.textContent = "周回可能数：無限";
+    outputtime.textContent = "周回可能時間：無限";
+    return;
+  }
+
+  let currentmorale = morale;
+  let totaltime = 0;
+  let laps = 0;
+
+  if (currentmorale > 75) {
+    currentmorale = 75;
+  }
+
+  while (currentmorale > 0) {
+    totaltime += time;
+    laps++;
+    currentmorale--;
+
+    if (mode === "terakoya") {
+      currentmorale += Math.floor(totaltime / 360) * 2;
+    } else {
+      currentmorale += Math.floor(totaltime / 360);
+    }
+
+    if (mode === "terakoya") {
+      currentmorale -= Math.floor((totaltime - time) / 360) * 2;
+    } else {
+      currentmorale -= Math.floor((totaltime - time) / 360);
+    }
+  }
+
+  let timetext = "";
+  const h = Math.floor(totaltime / 3600);
+  const m = Math.floor((totaltime % 3600) / 60);
+  const s = totaltime % 60;
+
+  if (h > 0) {
+    timetext += `${h}時間`;
+  }
+  if (h > 0 || m > 0) {
+    timetext += `${m}分`;
+  }
+  timetext += `${s}秒`;
+
+  outputlaps.textContent = `周回可能数：約${laps}周`;
+  outputtime.textContent = `周回可能時間：約${timetext}`;
+}
